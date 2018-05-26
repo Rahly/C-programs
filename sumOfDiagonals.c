d4f5317a4892a60c5dd97f76b5bf7671e66aadc3
Rahly <karol.oleksy97@gmail.com>
2018-05-26 12:11:12 +0200

    Adding function to dynamic allocate, fill and free Array

--- sumOfDiagonals.c
+++ sumOfDiagonals.c
@@ -2,7 +2,75 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int ** allocate(int height, int width);
+void fillArray(int height, int width, int **array);
+void freeArray(int width, int ***array);
+
+
 
 int main(int argc, char *argv[]) {
+	
+	int height;
+	int width;
+	int Ax, Ay;
+
+	
+	printf("Podaj wymiary macierzy A (MxN)\n");
+	scanf("%d%d", &height, &width);
+	
+	int **array = allocate(height, width);
+	
 	return 0;
 }
+
+
+int ** allocate(int height, int width){
+
+	int widthCounter;
+	int **array = NULL;
+	
+	array = (int**)calloc(height, sizeof(int));
+		
+		for(widthCounter = 0; widthCounter < width; widthCounter++){
+			
+			*(array + widthCounter) = (int*)calloc(width, sizeof(int));
+		}
+
+	return array;
+}
+
+void freeArray(int width, int ***array){
+	
+	int widthCounter;
+	
+	for(widthCounter = 0; widthCounter< width; widthCounter++){
+		
+		free(*((*array)+widthCounter));
+	}
+	
+	free(*array);
+	*array = NULL;
+	
+	return;
+}
+
+void fillArray(int height, int width, int **array){
+	
+	int widthCounter;
+	int heightCounter;
+	
+
+	
+	for(heightCounter = 0; heightCounter< height; heightCounter++){
+		
+		for(widthCounter = 0; widthCounter< width; widthCounter++){
+			
+			*(*(array+heightCounter)+widthCounter) = (1+rand()%10);
+		}
+	}
+	
+	return;
+}
+
